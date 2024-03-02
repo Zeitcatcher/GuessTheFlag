5be02de9de473971dbe929fe485f87c71e4f1a4c
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-03-01 18:16:56 -0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Arseniy Oksenoyt on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
